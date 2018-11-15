ef0555ca06b57805478e245f782a0215c56d3ee8
Iryna <iryna.finogenova@gmail.com>
2018-11-15 15:43:00 -0600

    Counter component, Battery upgrade + Car styles

--- src/containers/Battery.js
+++ src/containers/Battery.js
@@ -3,6 +3,7 @@ import './Battery.css';
 import Car from '../components/Car/Car';
 import Stats from '../components/Stats/Stats';
 import Notice from '../components/Notice/Notice';
+import Counter from '../components/Counter/Counter';
 import { getModelData } from '../services/BatteryService';
 
 class Battery extends React.Component {
@@ -11,7 +12,9 @@ class Battery extends React.Component {
 
         this.calculateStats = this.calculateStats.bind(this);
         this.statsUpdate = this.statsUpdate.bind(this);
-        
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+        this.updateCounterState = this.updateCounterState.bind(this);
         this.state = {
                 carstats: [],
                 config: {
@@ -42,21 +45,84 @@ class Battery extends React.Component {
             carstats: this.calculateStats(carModels, this.state.config)
         })  
     }
+
+    updateCounterState(title, newValue) {
+        const config = { ...this.state.config };
+        // update config state with new value
+        title === 'Speed' ? config['speed'] = newValue : config['temperature'] = newValue;
+        // update our state
+        this.setState({ config }, () => {this.statsUpdate()});
+    }
+
+    increment(e, title) {
+        e.preventDefault();
+        let currentValue, maxValue, step;
+        const { speed, temperature } = this.props.counterDefaultVal;
+        if (title === 'Speed') {
+            currentValue = this.state.config.speed;
+            maxValue = speed.max;
+            step = speed.step;
+        } else {
+            currentValue = this.state.config.temperature;
+            maxValue = temperature.max;
+            step = temperature.step;
+        }
+
+        if (currentValue < maxValue) {
+            const newValue = currentValue + step;
+            this.updateCounterState(title, newValue);
+        }
+    }
+
+    decrement(e, title) {
+        e.preventDefault();
+        let currentValue, minValue, step;
+        const { speed, temperature } = this.props.counterDefaultVal;
+        if (title === 'Speed') {
+            currentValue = this.state.config.speed;
+            minValue = speed.min;
+            step = speed.step;
+        } else {
+            currentValue = this.state.config.temperature;
+            minValue = temperature.min;
+            step = temperature.step;
+        }
+
+        if (currentValue > minValue) {
+            const newValue = currentValue - step;
+            this.updateCounterState(title, newValue);
+        }
+    }
     
     componentDidMount() {
         this.statsUpdate(); 
     }
       
-    render() {
-    const { config, carstats } = this.state;
+    render() {	
         return (
-            <form className="tesla-battery">
+          <form className="tesla-battery">
             <h1>Range Per Charge</h1>
-            <Car wheelsize={config.wheels}/>
-            <Stats carstats={carstats}/>
+            <Car wheelsize={this.state.config.wheels} />
+            <Stats carstats={this.state.carstats} />
+            <div className="tesla-controls cf">
+              <Counter
+                currentValue={this.state.config.speed}
+                initValues={this.props.counterDefaultVal.speed}
+                increment={this.increment}
+                decrement={this.decrement}
+              />
+              <div className="tesla-climate-container cf">
+                <Counter
+                  currentValue={this.state.config.temperature}
+                  initValues={this.props.counterDefaultVal.temperature}
+                  increment={this.increment}
+                  decrement={this.decrement}
+                />
+              </div>
+            </div>
             <Notice />
-            </form>
-        )
+        </form>
+      )
     }
 }
 
