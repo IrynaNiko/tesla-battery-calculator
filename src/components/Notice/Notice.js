a0dba7c590e6619fae94ef22c2e7a715065e8d0a
Iryna <iryna.finogenova@gmail.com>
2018-11-25 16:41:18 -0600

    Some styles for Notice component

--- src/components/Notice/Notice.js
+++ src/components/Notice/Notice.js
@@ -8,6 +8,7 @@ const Notice = () => (
       on your particular use conditions. See how particular use conditions
       may affect your range in our simulation model.
     </p>
+    <br/>
     <p>
       Vehicle range may vary depending on the vehicle configuration,
       battery age and condition, driving style and operating, environmental
